f30e955ad3456363e86e660c0e794788c564a265
Snownee <mjy523@126.com>
2021-07-27 13:45:23 +0800

    port

--- src/main/java/snownee/clearview/ClearView.java
+++ src/main/java/snownee/clearview/ClearView.java
@@ -1,10 +1,11 @@
 package snownee.clearview;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.entity.player.ClientPlayerEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.potion.Effects;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.tags.FluidTags;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.material.FogType;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.EntityViewRenderEvent;
@@ -18,37 +19,42 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 @EventBusSubscriber(Dist.CLIENT)
 public final class ClearView {
 
-    @SubscribeEvent
-    @OnlyIn(Dist.CLIENT)
-    public static void fogDensity(EntityViewRenderEvent.FogDensity event) {
-        ClientPlayerEntity player = Minecraft.getInstance().player;
-        if (player == null) {
-            return;
-        }
-        if (player.isCreative() || player.isSpectator()) {
-            event.setDensity(0f);
-            event.setCanceled(true);
-        } else if (canApply(player)) {
-            event.setDensity(0.03f);
-            event.setCanceled(true);
-        }
-    }
+	@SubscribeEvent
+	@OnlyIn(Dist.CLIENT)
+	public static void fogDensity(EntityViewRenderEvent.FogDensity event) {
+		LocalPlayer player = Minecraft.getInstance().player;
+		if (player == null) {
+			return;
+		}
+		if (event.getRenderer().getMainCamera().getFluidInCamera() == FogType.NONE) {
+			return;
+		}
+		float f = event.getRenderer().getRenderDistance();
+		if (player.isCreative() || player.isSpectator()) {
+			event.setDensity(f * 2f);
+			event.setCanceled(true);
+		} else if (canApply(player)) {
+			event.setDensity(f);
+			event.setCanceled(true);
+		}
+	}
 
-    @SubscribeEvent
-    @OnlyIn(Dist.CLIENT)
-    public static void renderOverlay(RenderBlockOverlayEvent event) {
-        if (event.getOverlayType() != OverlayType.FIRE) {
-            return;
-        }
-        if (event.getPlayer().isCreative()) {
-            event.setCanceled(true);
-        } else if (canApply(event.getPlayer())) {
-            event.getMatrixStack().translate(0, -0.25, 0);
-        }
-    }
+	@SubscribeEvent
+	@OnlyIn(Dist.CLIENT)
+	public static void renderOverlay(RenderBlockOverlayEvent event) {
+		if (event.getOverlayType() != OverlayType.FIRE) {
+			return;
+		}
+		if (event.getPlayer().isCreative()) {
+			event.setCanceled(true);
+		} else if (canApply(event.getPlayer())) {
+			event.getMatrixStack().translate(0, -0.25, 0);
+		}
+	}
 
-    public static boolean canApply(PlayerEntity player) {
-        return player.areEyesInFluid(FluidTags.LAVA) && player.isPotionActive(Effects.FIRE_RESISTANCE);
-    }
+	@OnlyIn(Dist.CLIENT)
+	public static boolean canApply(Player player) {
+		return player.isEyeInFluid(FluidTags.LAVA) && player.hasEffect(MobEffects.FIRE_RESISTANCE);
+	}
 
 }
